a7e4689ad4bd2101cb40f0e1b1db509af09a390a
ehmicky <ehmicky@gmail.com>
2019-08-08 10:00:00 +0200

    Separate all-node-versions

--- src/versions.js
+++ src/versions.js
@@ -1,8 +1,8 @@
+import allNodeVersions from 'all-node-versions'
 import { maxSatisfying } from 'semver'
 
 import { getCachedVersions, cacheVersions, VERSIONS_CACHE } from './cache.js'
 import { cleanupOnError } from './cleanup.js'
-import { fetchUrl } from './fetch.js'
 
 // Retrieve the Node version matching a specific `versionRange`
 export const getVersion = async function(versionRange) {
@@ -25,25 +25,9 @@ const getVersions = async function(versionRange) {
     return cachedVersions
   }
 
-  const versions = await fetchVersions()
+  const versions = await allNodeVersions()
 
   await cleanupOnError(() => cacheVersions(versions), VERSIONS_CACHE)
 
   return versions
-}
-
-// Fetch all available Node versions by making a HTTP request to Node website
-// Versions are already sorted from newest to oldest
-const fetchVersions = async function() {
-  const response = await fetchUrl(INDEX_URL)
-  const index = await response.json()
-  const versions = index.map(getVersionField)
-  return versions
-}
-
-const INDEX_URL = 'https://nodejs.org/dist/index.json'
-
-const getVersionField = function({ version }) {
-  // Remove the leading `v`
-  return version.slice(1)
 }
