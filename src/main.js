ee38045ae2dd3e8325a0f6640147307f830ce897
ehmicky <ehmicky@gmail.com>
2019-08-09 10:00:00 +0200

    Use global cache directory

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 import { spawn } from 'child_process'
 
 import getNode from 'get-node'
-import findCacheDir from 'find-cache-dir'
+import globalCacheDir from 'global-cache-dir'
 import pEvent from 'p-event'
 
 import { validateInput } from './validate.js'
-
-export const CACHE_DIR = findCacheDir({ name: 'nve', create: true })
 
 // Forwards `args` to another node instance of a specific `versionRange`
 const nve = async function(versionRange, args = []) {
   validateInput(args)
 
   // Download the Node.js binary
-  const nodePath = await getNode(versionRange, CACHE_DIR)
+  const cacheDir = await globalCacheDir(CACHE_DIR)
+  const nodePath = await getNode(versionRange, cacheDir)
 
   const { code, signal } = await runNodeProcess(nodePath, args)
   return { code, signal }
 }
+
+const CACHE_DIR = 'nve'
 
 // Forward arguments to another node binary located at `nodePath`.
 // We also forward standard streams and exit code.
