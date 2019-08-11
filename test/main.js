12bef9f75e78b2803e2a693ad996bd31b66008a0
ehmicky <ehmicky@gmail.com>
2019-08-11 10:00:00 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -7,10 +7,10 @@ import nve from '../src/main.js'
 
 const TEST_VERSION = '6.0.0'
 
-const runCli = async function(args = '--version') {
+const runCli = async function(args = '--version', versionRange = TEST_VERSION) {
   const binPath = await getBinPath()
   const { stdout, stderr, exitCode: code } = await execa.command(
-    `${binPath} ${TEST_VERSION} ${args}`,
+    `${binPath} ${versionRange} ${args}`,
     { reject: false },
   )
   return { stdout, stderr, code }
@@ -50,6 +50,13 @@ test('Forward signal | programmatic', async t => {
   const { signal } = await nve(TEST_VERSION, ['-e', '""'])
 
   t.is(signal, null)
+})
+
+test('Invalid arguments | CLI', async t => {
+  const { stderr, code } = await runCli('', 'invalid_version')
+
+  t.is(code, 1)
+  t.true(stderr.includes('invalid_version'))
 })
 
 each(
