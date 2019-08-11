77a82931548001d098ef954e16a6cfec78fb7c9c
ehmicky <ehmicky@gmail.com>
2019-08-11 10:00:00 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -1,5 +1,21 @@
-import test from 'ava'
+import { execFile } from 'child_process'
+import { promisify } from 'util'
 
-test('Dummy test', t => {
-  t.pass()
+import test from 'ava'
+import { getBinPath } from 'get-bin-path'
+
+const pExecFile = promisify(execFile)
+
+const TEST_VERSION = '6.0.0'
+
+test('Success', async t => {
+  const binPath = await getBinPath()
+  const { stdout } = await pExecFile('node', [
+    binPath,
+    TEST_VERSION,
+    '--version',
+  ])
+  const stdoutA = stdout.trim()
+
+  t.is(stdoutA, `v${TEST_VERSION}`)
 })
