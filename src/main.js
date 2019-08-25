8c3f756914134481a866874d18f0b2faf292ba95
ehmicky <ehmicky@gmail.com>
2019-08-25 16:55:36 +0200

    Upgrade get-node 2.0.1 -> 3.0.0

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { spawn } from 'child_process'
 import { env } from 'process'
 
 import getNode from 'get-node'
-import globalCacheDir from 'global-cache-dir'
 import pEvent from 'p-event'
 
 import { validateInput } from './validate.js'
@@ -19,15 +18,10 @@ export const nve = async function(versionRange, args = []) {
 
 // Download the Node.js binary
 export const getPath = async function(versionRange, progress) {
-  const cacheDir = await globalCacheDir(CACHE_DIR)
   const progressA = progress !== false && env.NVE_PROGRESS !== '0'
-  const nodePath = await getNode(versionRange, cacheDir, {
-    progress: progressA,
-  })
+  const nodePath = await getNode(versionRange, { progress: progressA })
   return nodePath
 }
-
-const CACHE_DIR = 'nve'
 
 // Forward arguments to another node binary located at `nodePath`.
 // We also forward standard streams and exit code.
