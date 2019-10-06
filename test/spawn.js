83f6122684c84e62daeabeebd0975f31ef5bc021
ehmicky <ehmicky@gmail.com>
2019-10-06 22:57:41 +0200

    Improve tests

--- test/spawn.js
+++ test/spawn.js
@@ -1,5 +1,4 @@
 import { ChildProcess } from 'child_process'
-import { version } from 'process'
 
 import test from 'ava'
 import { each } from 'test-each'
@@ -7,7 +6,7 @@ import pathKey from 'path-key'
 
 import nve from '../src/main.js'
 
-import { TEST_VERSION } from './helpers/versions.js'
+import { TEST_VERSION, HELPER_VERSION } from './helpers/versions.js'
 
 test('Forward child process | programmatic', async t => {
   const { childProcess } = await nve(TEST_VERSION, 'node', ['-p', '"test"'])
@@ -39,14 +38,14 @@ each(
 )
 
 test('Can fire global binaries', async t => {
-  const { childProcess } = await nve(version, 'npm', ['--version'])
+  const { childProcess } = await nve(HELPER_VERSION, 'npm', ['--version'])
   const { stdout } = await childProcess
 
   t.true(stdout !== '')
 })
 
 test('Can fire local binaries', async t => {
-  const { childProcess } = await nve(version, 'ava', ['--version'], {
+  const { childProcess } = await nve(HELPER_VERSION, 'ava', ['--version'], {
     spawn: { env: { [pathKey()]: '' }, preferLocal: true },
   })
   const { stdout } = await childProcess
@@ -55,7 +54,7 @@ test('Can fire local binaries', async t => {
 })
 
 test('Can disable firing local binaries', async t => {
-  const { childProcess } = await nve(version, 'ava', ['--version'], {
+  const { childProcess } = await nve(HELPER_VERSION, 'ava', ['--version'], {
     spawn: { env: { [pathKey()]: '' }, preferLocal: false, stdio: 'ignore' },
   })
   const { exitCode } = await t.throwsAsync(childProcess)
@@ -64,7 +63,7 @@ test('Can disable firing local binaries', async t => {
 })
 
 test('Can specify both preferLocal and cwd options', async t => {
-  const { childProcess } = await nve(version, 'ava', ['--version'], {
+  const { childProcess } = await nve(HELPER_VERSION, 'ava', ['--version'], {
     spawn: {
       env: { [pathKey()]: '' },
       preferLocal: true,
