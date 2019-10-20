b01f6f31e2d166decb6a98803e1aced04f669d17
ehmicky <ehmicky@gmail.com>
2019-10-20 23:45:30 +0200

    Add more tests

--- test/cli.js
+++ test/cli.js
@@ -1,9 +1,10 @@
-import { platform } from 'process'
+import { platform, env } from 'process'
 
 import test from 'ava'
 import { each } from 'test-each'
 import readPkgUp from 'read-pkg-up'
 import isCi from 'is-ci'
+import hasAnsi from 'has-ansi'
 
 import { TEST_VERSION, OLD_TEST_VERSION } from './helpers/versions.js'
 import { runCli, runCliSerial } from './helpers/run.js'
@@ -149,6 +150,16 @@ v${TEST_VERSION}
 
 v${TEST_VERSION}`,
   )
+})
+
+test.serial('Prints headers in colors | CLI runCliSerial', async t => {
+  // eslint-disable-next-line fp/no-mutation
+  env.FORCE_COLOR = '1'
+  const { stderr } = await runCliSerial('', TEST_VERSION, 'node --version')
+  // eslint-disable-next-line fp/no-delete
+  delete env.FORCE_COLOR
+
+  t.true(hasAnsi(stderr))
 })
 
 test('Prints headers in correct order | CLI runCliSerial', async t => {
