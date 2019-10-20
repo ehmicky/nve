e143e936003d51c676725a1a6d8d479c91c026d5
ehmicky <ehmicky@gmail.com>
2019-10-20 11:39:10 +0200

    Improve tests

--- test/cli/parse.js
+++ test/cli/parse.js
@@ -2,26 +2,15 @@ import test from 'ava'
 import { each } from 'test-each'
 
 import { TEST_VERSION } from '../helpers/versions.js'
-import { runCli, runCliSerial } from '../helpers/run.js'
-
-each(
-  [['', ''], [TEST_VERSION, 'invalid_binary'], ['invalid_version', 'node']],
-  [runCli, runCliSerial],
-  ({ title }, [versionRange, command], run) => {
-    test(`Invalid arguments | CLI ${title}`, async t => {
-      const { exitCode, stderr } = await run('', versionRange, command)
-
-      t.not(exitCode, 0)
-      t.true(stderr !== '')
-    })
-  },
-)
+import { runCli } from '../helpers/run.js'
 
 each(
   [
     ['', '', ''],
     ['--shell', '', ''],
     ['--shell', '', 'node --version'],
+    ['', 'invalid_version', 'node --version'],
+    ['--shell', 'invalid_version', 'node --version'],
     ['', TEST_VERSION, 'invalid'],
   ],
   ({ title }, [options, versionRange, command]) => {
