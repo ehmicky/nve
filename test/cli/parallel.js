ccaa01a41d32cfe7e2ba174db37f802ce08c30e2
ehmicky <ehmicky@gmail.com>
2019-10-20 21:37:37 +0200

    Add tests

--- test/cli/parallel.js
+++ test/cli/parallel.js
@@ -1,9 +1,10 @@
 import test from 'ava'
+import { each } from 'test-each'
 
 import { TEST_VERSION, OLD_TEST_VERSION } from '../helpers/versions.js'
-import { runCli } from '../helpers/run.js'
+import { runCli, runCliSerial, runCliParallel } from '../helpers/run.js'
 
-test(`Forward exit code and output on late failure | runCliParallel`, async t => {
+test('Forward exit code and output on late failure | runCliParallel', async t => {
   const { exitCode, stdout, stderr } = await runCli(
     '--parallel --continue',
     `${TEST_VERSION} ${OLD_TEST_VERSION}`,
@@ -23,7 +24,7 @@ Node ${OLD_TEST_VERSION} failed with exit code 1`),
   )
 })
 
-test(`--continue | runCliParallel`, async t => {
+test('--continue | runCliParallel', async t => {
   const { exitCode, stdout, stderr } = await runCli(
     '--parallel --continue',
     `${OLD_TEST_VERSION} ${TEST_VERSION}`,
@@ -42,3 +43,20 @@ Node ${OLD_TEST_VERSION} failed with exit code 1
   t.true(stdout.startsWith('[eval]:1'))
   t.true(stdout.endsWith('<Buffer >'))
 })
+
+each(
+  [
+    { run: runCliSerial, minimum: 2e3, maximum: 4e3 },
+    { run: runCliParallel, minimum: 1e3, maximum: 2e3 },
+  ],
+  ({ title }, { run, minimum, maximum }) => {
+    test.serial(`Run in parallel/serial | ${title}`, async t => {
+      const start = Date.now()
+      await run('', TEST_VERSION, 'node -e setTimeout(()=>{},1e3)')
+      const duration = Date.now() - start
+
+      t.true(duration >= minimum)
+      t.true(duration <= maximum)
+    })
+  },
+)
