273df1a801a63a8021adfa718812b5c6890b18a6
ehmicky <ehmicky@gmail.com>
2019-10-20 21:38:30 +0200

    Add test

--- test/cli/cleanup.js
+++ test/cli/cleanup.js
@@ -1,8 +1,8 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { TEST_VERSION } from '../helpers/versions.js'
-import { runCliSerial, runCliParallel } from '../helpers/run.js'
+import { OLD_TEST_VERSION, TEST_VERSION } from '../helpers/versions.js'
+import { runCli, runCliSerial, runCliParallel } from '../helpers/run.js'
 
 each([runCliSerial, runCliParallel], ({ title }, run) => {
   test(`Works with early failures | ${title}`, async t => {
@@ -11,3 +11,28 @@ each([runCliSerial, runCliParallel], ({ title }, run) => {
     t.is(exitCode, 1)
   })
 })
+
+each(
+  [runCliSerial, runCliParallel],
+  [
+    { opts: '', minimum: 0, maximum: 1e3 },
+    { opts: '--continue', minimum: 1e3, maximum: 2e3 },
+  ],
+  ({ title }, run, { opts, minimum, maximum }) => {
+    test.serial(
+      `Does not run other processes on failures | ${title}`,
+      async t => {
+        const start = Date.now()
+        await runCli(
+          opts,
+          `${OLD_TEST_VERSION} ${TEST_VERSION}`,
+          'node -e Buffer.from("")&&setTimeout(()=>{},1e3)',
+        )
+        const duration = Date.now() - start
+
+        t.true(duration >= minimum)
+        t.true(duration <= maximum)
+      },
+    )
+  },
+)
