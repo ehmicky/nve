063234d68cad05157280a602e5be77b4db91179a
ehmicky <ehmicky@gmail.com>
2019-10-20 21:41:51 +0200

    Add tests

--- test/cli/error.js
+++ test/cli/error.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { TEST_VERSION } from '../helpers/versions.js'
+import { OLD_TEST_VERSION, TEST_VERSION } from '../helpers/versions.js'
 import { runCli, runCliSerial, runCliParallel } from '../helpers/run.js'
 
 each([runCli, runCliSerial, runCliParallel], ({ title }, run) => {
@@ -30,4 +30,38 @@ Node ${TEST_VERSION} failed with exit code 2`,
       ),
     )
   })
+})
+
+test(`Prints aborted message if late | runCliParallel`, async t => {
+  const { stderr } = await runCli(
+    '--parallel',
+    `${TEST_VERSION} ${OLD_TEST_VERSION}`,
+    'node -p Buffer.from("")&&setTimeout(()=>{},1e9)',
+  )
+
+  t.is(
+    stderr,
+    `<>  Node ${TEST_VERSION}
+
+Node ${TEST_VERSION} aborted
+
+ <>  Node ${OLD_TEST_VERSION}
+
+Node ${OLD_TEST_VERSION} failed with exit code 1`,
+  )
+})
+
+test(`Prints no aborted message if early | runCliParallel`, async t => {
+  const { stderr } = await runCli(
+    '--parallel',
+    `${OLD_TEST_VERSION} ${TEST_VERSION}`,
+    'node -p Buffer.from("")&&setTimeout(()=>{},1e9)',
+  )
+
+  t.is(
+    stderr,
+    `<>  Node ${OLD_TEST_VERSION}
+
+Node ${OLD_TEST_VERSION} failed with exit code 1`,
+  )
 })
