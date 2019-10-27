0b60db59f037a6c816572f6d92813ba7592e4807
ehmicky <ehmicky@gmail.com>
2019-10-27 23:08:15 +0100

    Fix code duplication

--- test/bin/parse.js
+++ test/bin/parse.js
@@ -6,14 +6,14 @@ import { runCli } from '../helpers/run.js'
 each(
   [
     { options: '', versionRange: '', command: '' },
-    { options: '--shell', versionRange: '', command: '' },
     { options: '--shell', versionRange: '', command: 'node --version' },
-    { options: '', versionRange: 'invalid_version', command: 'node --version' },
+    { options: '--shell', versionRange: '', command: '' },
     {
       options: '--shell',
       versionRange: 'invalid_version',
       command: 'node --version',
     },
+    { options: '', versionRange: 'invalid_version', command: 'node --version' },
     { options: '', versionRange: '0.0.0', command: 'node --version' },
   ],
   ({ title }, { options, versionRange, command }) => {
