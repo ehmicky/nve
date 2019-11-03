7b561117ca6bf56dce1cde89b2ea7350b467647e
ehmicky <ehmicky@gmail.com>
2019-11-03 10:58:51 +0100

    Move file

--- examples/utils.sh
+++ examples/utils.sh
@@ -4,7 +4,7 @@
 dir="$(dirname "$BASH_SOURCE")"
 projectRoot="$(realpath "$dir/..")"
 binaryName="$(basename "$projectRoot")"
-pathToBinary="build/src/main.js"
+pathToBinary="build/src/bin.js"
 
 cd "$dir"
 
