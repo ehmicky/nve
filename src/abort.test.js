407eea9d9f50efad9092447db8064b2e2e1005cb
ehmicky <ehmicky@gmail.com>
2023-07-09 16:03:20 +0100

    Fix tests

--- src/abort.test.js
+++ src/abort.test.js
@@ -7,7 +7,7 @@ import { VERY_OLD_TEST_VERSION, TEST_VERSION } from './helpers/versions.test.js'
 const RECENT_ARCH_OPT = '--arch arm'
 
 each(['', '--parallel'], ({ title }, parallelFlag) => {
-  test(`Node.js binary failure | ${title}`, async (t) => {
+  test.serial(`Node.js binary failure | ${title}`, async (t) => {
     const { stdout, stderr, exitCode } = await runCli(
       `${RECENT_ARCH_OPT} ${parallelFlag}`,
       `${VERY_OLD_TEST_VERSION},${TEST_VERSION}`,
